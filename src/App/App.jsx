3e3022d3c48b16b057bafef8d7a6230996442931
Akula3331 <nlakula321@gmail.com>
2023-10-02 21:39:30 +0600

    new file:   public/images/datebg.jpg 	new file:   public/images/reibg.jpg 	modified:   public/index.html 	modified:   src/App/App.jsx 	new file:   src/components/BlockTime/BlockTime.jsx 	new file:   src/components/BlockTime/BlockTime.module.scss 	deleted:    src/components/timeBl/timeBl.jsx 	deleted:    src/components/timeBl/timeBl.module.scss

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,10 +1,11 @@
+import BlockTime from "../components/BlockTime/BlockTime";
 import "./App.scss";
 
 function App() {
     return (
         <div className="App">
             <h1>Hello World!</h1>
-            
+            <BlockTime/>
         </div>
     );
 }
