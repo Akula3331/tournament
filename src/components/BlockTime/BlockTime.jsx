b1650ea9adcb455a096d876d8a397d76a7910aac
Akula3331 <nlakula321@gmail.com>
2023-10-07 18:11:58 +0600

    modified:   package-lock.json 	modified:   package.json 	renamed:    public/icons/game controller.svg -> public/icons/gamecontroller.svg 	modified:   src/App/App.jsx 	modified:   src/components/BlockTime/BlockTime.jsx 	modified:   src/components/BlockTime/BlockTime.module.scss 	new file:   src/components/Card/Card.jsx 	renamed:    src/components/CardB/CardB.module.scss -> src/components/Card/Card.module.scss 	new file:   src/components/CountdownTimer/CountdownTimer.jsx 	new file:   src/components/CountdownTimer/CountdownTimer.module.scss 	new file:   src/components/TimeCon/TimeCon.jsx 	new file:   src/components/TimeCon/TimeCon.module.scss 	new file:   src/components/VsCon/VsCon.jsx 	renamed:    src/components/CardB/Cardb.jsx -> src/components/VsCon/VsCon.module.scss

--- src/components/BlockTime/BlockTime.jsx
+++ src/components/BlockTime/BlockTime.jsx
@@ -1,18 +1,62 @@
-import React from 'react'
-import cls from './BlockTime.module.scss'
+import React, { useState } from "react";
+import cls from "./BlockTime.module.scss";
+import { useSpring, animated } from 'react-spring';
 
-export default function BlockTime() {
+export default function BlockTime({data,dis,img,onToggle,isOpen,pNum,vs }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+  
+  const toggleSecondContainer = () => {
+   onToggle(); 
+  };
+  const toggleExpand = () => {
+    setIsExpanded((prevExpanded) => !prevExpanded);
+  };
+
+  // Создаем анимацию для появления/скрытия дополнительной информации
+  const expandAnimation = useSpring({
+    opacity: isOpen ? 1 : 0,
+    maxHeight: isOpen ? 1000 : 0,
+    overflow: 'hidden',
+    transform: `translateY(${isOpen ? 0 : -20}px)`,
+    config: {duration: 300}
+
+  });
+ 
+  const hasContentToDisplay = pNum && pNum.length > 0;
+ 
+  
   return (
-    <div>
-        <div className={cls.header}>
-            <p className={cls.headerItem}>Тип</p>
-            <p className={cls.headerItem}>Дисциплина</p>
-            <p className={cls.headerItem}>Дата</p>
+    <div className={cls.container}>
+      <div
+        className={`${cls.mainBlock} ${isExpanded ? cls.expanded : ""}`}
+        onClick={toggleSecondContainer}
+      >
+        <div className={cls.iconCon}>
+          <img src={img} alt="" className={cls.icon} />
         </div>
-        <div className={cls.con}>
-          <img src="/images/date" alt="" />
+        <div className={cls.line}></div>
+        <p className={cls.txt}>{dis}</p>
+        <div className={cls.line2}></div>
 
-        </div>
+        <p className={cls.num}>{data}</p>
+      </div>
+      
+      {hasContentToDisplay && (
+        <animated.div className={cls.additionalInfo} style={expandAnimation}>
+          <div className={cls.secCon}>
+            <div className={cls.secBlock}>
+              <p className={cls.snum}>{pNum[0]}</p>
+              <img src="/icons/vs.svg" alt="" />
+              <p className={cls.snum}>{pNum[1]}</p>
+            </div>
+            <div className={cls.secBlock}>
+              <p className={cls.snum}>{pNum[2]}</p>
+              <img src="/icons/vs.svg" alt="" />
+              <p className={cls.snum}>{pNum[3]}</p>
+            </div>
+          </div>
+        </animated.div>
+      )}
     </div>
-  )
+  );
 }
